363c6e556c5771a5efa439ebd9a3f18564612729
strohganoff <dstroh10@gmail.com>
2025-03-19 13:05:34 -0500

    Fix up EventListenerManager to reduce processing load of  method by using a Sentinal

--- streamdeck/event_listener.py
+++ streamdeck/event_listener.py
@@ -11,11 +11,24 @@ if TYPE_CHECKING:
     from collections.abc import Generator
     from typing import Any, ClassVar
 
+    from typing_extensions import TypeIs
+
     from streamdeck.models.events import EventBase
+
 
 
 logger = getLogger("streamdeck.event_listener")
 
+
+class _SENTINAL:
+    """A sentinel object used to signal the end of the event stream.
+
+    Not meant to be instantiated, but rather used as a singleton (e.g. `_SENTINAL`).
+    """
+    @classmethod
+    def is_sentinal(cls, event: str | bytes | type[_SENTINAL]) -> TypeIs[type[_SENTINAL]]:
+        """Check if an event is the sentinal object. Provided to enable better type-checking."""
+        return event is cls
 
 
 class EventListenerManager:
@@ -25,7 +38,7 @@ class EventListenerManager:
     This allows for us to listen for not only Stream Deck events, but also other events plugin-developer -defined events.
     """
     def __init__(self) -> None:
-        self.event_queue: Queue[str | bytes] = Queue()
+        self.event_queue: Queue[str | bytes | type[_SENTINAL]] = Queue()
         self.listeners_lookup_by_thread: dict[threading.Thread, EventListener] = {}
         self._running = False
 
@@ -60,7 +73,11 @@ class EventListenerManager:
 
         Listeners will check the running flag if implemented to stop listening.
         """
+        # Set the running flag to False to stop the listeners running in separate threads.
         self.running = False
+        # Push the sentinel to immediately unblock the queue.get() in event_stream.
+        self.event_queue.put(_SENTINAL)
+
         for thread in self.listeners_lookup_by_thread:
             self.listeners_lookup_by_thread[thread].stop()
             thread.join()
@@ -70,19 +87,19 @@ class EventListenerManager:
     def event_stream(self) -> Generator[str | bytes, None, None]:
         """Starts all registered listeners, sets the running flag to True, and yields events from the shared queue."""
         logger.info("Starting event stream.")
+        # Set the running flag to True and start the listeners in their separate threads.
         self.running = True
-
         for thread in self.listeners_lookup_by_thread:
             thread.start()
 
         try:
-            while self.running:
-                if not self.event_queue.empty():
-                    yield self.event_queue.get()
+            while True:
+                event = self.event_queue.get()
+                if _SENTINAL.is_sentinal(event):
+                    break  # Exit loop immediately if the sentinal is received
+                yield event
         finally:
             self.stop()
-
-
 
 
 class EventListener(ABC):
